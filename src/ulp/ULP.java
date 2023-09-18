c703079dbb38858d87747b9bbe183b1f74676a6e
KoliRamone <129777320+KoliRamone@users.noreply.github.com>
2023-09-18 12:16:23 -0300

    Menu agregado
    Abre menu al correr programa, falta conección a las otras tabs

--- src/ulp/ULP.java
+++ src/ulp/ULP.java
@@ -24,7 +24,7 @@ public class ULP {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        menu.
+        menu.NimbusLookAndFeel();
     }
     
 }
