d4b54d08c156f0d6363c939e90e3c34a36705cc9
KoliRamone <129777320+KoliRamone@users.noreply.github.com>
2023-09-19 11:09:14 -0300

    boton salir agregado

--- src/universidad/Vistas/menu.java
+++ src/universidad/Vistas/menu.java
@@ -182,9 +182,8 @@ public class menu extends javax.swing.JFrame {
     }//GEN-LAST:event_jFormAlumxMatActionPerformed
 
     private void jSalir_menuprincipalMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jSalir_menuprincipalMouseClicked
-       
-       
-        
+  
+       System.exit(0);    
     }//GEN-LAST:event_jSalir_menuprincipalMouseClicked
 
     /**
